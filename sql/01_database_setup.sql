247d347f0fb3761c324e35aeaf8268443ffce701
Anmol Sharma <anmolsharma4777@gmail.com>
2026-05-25 18:05:23 +0530

    Added customer exploration queries

--- sql/01_database_setup.sql
+++ sql/01_database_setup.sql
@@ -6,4 +6,27 @@ CREATE DATABASE ecommerce_analytics;
 USE ecommerce_analytics;
 
 -- Imported customers dataset
--- Verified sucessful import
+-- Verified sucessful 
+
+-- Verified customer table structure
+
+DESCRIBE olist_customers_dataset;
+
+-- Previewed customer data
+
+SELECT *
+FROM olist_customers_dataset
+LIMIT 10;
+
+-- Counted total customers
+
+SELECT COUNT(*) AS total_customers
+FROM olist_customers_dataset;
+
+-- Customer distribution by state
+
+SELECT customer_state,
+       COUNT(*) AS total_customers
+FROM olist_customers_dataset
+GROUP BY customer_state
+ORDER BY total_customers DESC;
